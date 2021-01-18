d47ccb09326528d59017280762403240aaef6d71
Nurtilek2005 <nurtileks2005@gmail.com>
2021-01-18 19:31:23 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+/* ----------Задание №1---------- */
+
+// let number = prompt('Введите число');
+// let validatePromptNumber = function (number) {
+//   if(number === null) {
+//     return 'Вы отменили ввод.';
+//   }
+//   if(isNaN(number) || number === '') {
+//     return 'Введите корректные данные!';
+//   }
+//   if(number > 0) {
+//     return 'Вы ввели положительное число';
+//   } else if(number < 0) {
+//     return 'Вы ввели отрицательное число';
+//   } else {
+//     return 'Число равно 0';
+//   }
+// }
+
+// let result = validatePromptNumber(number);
+
+// alert(result);
+
+/* ----------Задание №2---------- */
+// let eggsCount = parseInt( prompt('Сколько яиц нужно сварить') );
+// let time = 0;
+// let zakaz = function (eggsCount, time) {
+//   for(let i = 0; i < eggsCount; i++) {
+//     if(0 === i % 8) {
+//       time += 5;
+//     }
+//   }
+//   if(isNaN(eggsCount) || eggsCount === '') {
+//     return 'Введите корректные данные';
+//   } else {
+//     return `Для варки ${eggsCount} яиц нужно ${time} минут`;
+//   }
+// }
+// let result = zakaz(eggsCount, time);
+
+// console.log(result);
+
+
+/* ----------Задание №3---------- */
+let getEvenSum = function() {
+  for(let i = 0; i < getEvenSum.length; i++) {
+    s
+    return 'OK';
+  }
+}
+let result = getEvenSum(4, 3, 1, 2, 5, 10, 6, 7, 8);
+console.log(result)
+
+
+/* ----------Задание №4---------- */
+let checkExam = function (reshenie, otvetUchenika) {
+  
+}
